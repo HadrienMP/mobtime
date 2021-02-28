ae7f81c423af7f3a7ca227df489f7a264ca28ec3
hadrienmp <public@hadrienmp.fr>
2021-02-28 11:23:12 +0100

    Join the room when accessing the mob page directly

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -5,9 +5,6 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Lib.BatchMsg
 import Lib.Toast
-import Login
-import Mob.Main
-import Out.Commands
 import Out.Events
 import Out.EventsMapping as EventsMapping exposing (EventsMapping)
 import Pages
@@ -37,8 +34,6 @@ main =
 
 type alias Model =
     { session : Pages.Session
-    , userPreferences : UserPreferences.Model
-    , pageModel : Pages.PageModel
     , toasts : Lib.Toast.Toasts
     }
 
@@ -48,13 +43,14 @@ init maybePreference url key =
     let
         userPreference =
             Maybe.withDefault UserPreferences.default maybePreference
+
+        ( session, command ) =
+            Pages.init key url userPreference
     in
-    ( { session = Pages.Session key url
-      , userPreferences = userPreference
-      , pageModel = Pages.pageOf url userPreference
+    ( { session = session
       , toasts = Lib.Toast.init
       }
-    , Cmd.none
+    , command |> Cmd.map PageMsg
     )
 
 
@@ -65,19 +61,18 @@ init maybePreference url key =
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
-    | LoginMsg Login.Msg
-    | MobMsg Mob.Main.Msg
+    | PageMsg Pages.Msg
     | Batch (List Msg)
     | ToastMsg Lib.Toast.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case ( model.pageModel, msg ) of
-        ( _, Batch messages ) ->
+    case msg of
+        Batch messages ->
             Lib.BatchMsg.update messages model update
 
-        ( _, LinkClicked urlRequest ) ->
+        LinkClicked urlRequest ->
             case urlRequest of
                 Browser.Internal url ->
                     ( model, Pages.pushUrl url model.session )
@@ -85,49 +80,32 @@ update msg model =
                 Browser.External href ->
                     ( model, Nav.load href )
 
-        ( _, UrlChanged url ) ->
-            let
-                ( session, pageModel, pageCommand ) =
-                    Pages.urlChanged url model.session model.userPreferences
-
-            in
-            ( { model | session = session, pageModel = pageModel }
-            , pageCommand
-            )
-
-        ( Pages.LoginModel loginModel, LoginMsg loginMsg ) ->
-            Login.update loginModel loginMsg model.session.key
+        UrlChanged url ->
+            Pages.urlChanged url model.session
                 |> Tuple.mapBoth
-                    (\it -> { model | pageModel = Pages.LoginModel it })
-                    (Cmd.map LoginMsg)
+                    (\session -> { model | session = session })
+                    (Cmd.map PageMsg)
 
-        ( Pages.MobModel mobModel, MobMsg mobMsg ) ->
+        PageMsg pageMsg ->
             let
-                mobResult =
-                    Mob.Main.update mobMsg mobModel
+                ( session, command, toasts ) =
+                    Pages.update pageMsg model.session
 
-                mobCommand =
-                    Cmd.map MobMsg mobResult.command
-
-                ( toasts, commands ) =
-                    Lib.Toast.add mobResult.toast model.toasts
-                        |> Tuple.mapSecond (List.map (Cmd.map ToastMsg))
+                ( allToasts, commands ) =
+                    Lib.Toast.add toasts model.toasts
             in
             ( { model
-                | pageModel = Pages.MobModel mobResult.model
-                , toasts = toasts
+                | session = session
+                , toasts = allToasts
               }
-            , Cmd.batch <| mobCommand :: commands
+            , Cmd.batch <| (command |> Cmd.map PageMsg) :: (commands |> List.map (Cmd.map ToastMsg))
             )
 
-        ( _, ToastMsg toastMsg ) ->
+        ToastMsg toastMsg ->
             Lib.Toast.update toastMsg model.toasts
                 |> Tuple.mapBoth
                     (\it -> { model | toasts = it })
                     (Cmd.map ToastMsg)
-
-        _ ->
-            ( model, Cmd.none )
 
 
 
@@ -137,7 +115,7 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
-        [ Mob.Main.subscriptions |> Sub.map MobMsg
+        [ Pages.subscriptions |> Sub.map PageMsg
         , Out.Events.events (dispatch eventsMapping)
         ]
 
@@ -150,7 +128,7 @@ dispatch mapping event =
 eventsMapping : EventsMapping Msg
 eventsMapping =
     EventsMapping.batch
-        [ EventsMapping.map MobMsg Mob.Main.eventsMapping
+        [ EventsMapping.map PageMsg Pages.eventsMapping
         , EventsMapping.map ToastMsg Lib.Toast.eventsMapping
         ]
 
@@ -161,29 +139,9 @@ eventsMapping =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = pageTitle model
+    { title = Pages.pageTitle model.session
     , body =
-        [ pageBody model
+        [ Pages.pageBody model.session |> Html.map PageMsg
         , Lib.Toast.view model.toasts |> Html.map ToastMsg
         ]
     }
-
-
-pageTitle : Model -> String
-pageTitle model =
-    case model.pageModel of
-        Pages.LoginModel _ ->
-            Login.title
-
-        Pages.MobModel mobModel ->
-            Mob.Main.pageTitle mobModel
-
-
-pageBody : Model -> Html Msg
-pageBody model =
-    case model.pageModel of
-        Pages.LoginModel loginModel ->
-            Login.view loginModel |> Html.map LoginMsg
-
-        Pages.MobModel mobModel ->
-            Mob.Main.view mobModel model.session.url |> Html.map MobMsg
--- src/elm/Pages.elm
+++ src/elm/Pages.elm
@@ -1,9 +1,11 @@
 module Pages exposing (..)
 
 import Browser.Navigation as Nav
-import Lib.Toast
+import Html exposing (Html)
+import Lib.Toast exposing (Toasts)
 import Login
 import Mob.Main
+import Out.EventsMapping as EventsMapping exposing (EventsMapping)
 import Url
 import Url.Parser as UrlParser exposing ((</>), Parser, map, oneOf, s, top)
 import UserPreferences
@@ -13,14 +15,23 @@ type PageModel
     = LoginModel Login.Model
     | MobModel Mob.Main.Model
 
+
 type Page
     = Login
     | Mob String
 
+
 type alias Session =
     { key : Nav.Key
     , url : Url.Url
+    , pageModel : PageModel
+    , userPreferences : UserPreferences.Model
     }
+
+
+type Msg
+    = LoginMsg Login.Msg
+    | MobMsg Mob.Main.Msg
 
 
 pushUrl : Url.Url -> Session -> Cmd msg
@@ -28,31 +39,34 @@ pushUrl url session =
     Nav.pushUrl session.key (Url.toString url)
 
 
-urlChanged : Url.Url -> Session -> UserPreferences.Model -> (Session, PageModel, Cmd msg)
-urlChanged url session userPreferences =
+urlChanged : Url.Url -> Session -> ( Session, Cmd Msg )
+urlChanged url session =
+    init session.key url session.userPreferences
+
+
+init : Nav.Key -> Url.Url -> UserPreferences.Model -> ( Session, Cmd Msg )
+init key url userPreferences =
     let
-        model = pageOf url userPreferences
+        ( page, command ) =
+            initPage url userPreferences
     in
-    ({ session | url = url }, model, Cmd.none)
+    ( Session key url page userPreferences, command )
 
 
-pageOf : Url.Url -> UserPreferences.Model -> PageModel
-pageOf url userPreferences=
+initPage : Url.Url -> UserPreferences.Model -> ( PageModel, Cmd Msg )
+initPage url userPreferences =
     UrlParser.parse route url
         |> Maybe.withDefault Login
         |> (\page ->
-            case page of
-                Login ->
-                    Login.init
-                    |> Tuple.first
-                    |> LoginModel
+                case page of
+                    Login ->
+                        Login.init
+                            |> Tuple.mapBoth LoginModel (Cmd.map LoginMsg)
 
-
-                Mob name ->
-                    Mob.Main.init name userPreferences
-                    |> Tuple.first
-                    |> MobModel
-        )
+                    Mob name ->
+                        Mob.Main.init name userPreferences
+                            |> Tuple.mapBoth MobModel (Cmd.map MobMsg)
+           )
 
 
 route : Parser (Page -> c) c
@@ -61,3 +75,59 @@ route =
         [ map Login top
         , map Mob (s "mob" </> UrlParser.string)
         ]
+
+
+update : Msg -> Session -> ( Session, Cmd Msg, Toasts )
+update msg session =
+    case ( session.pageModel, msg ) of
+        ( LoginModel loginModel, LoginMsg loginMsg ) ->
+            Login.update loginModel loginMsg session.key
+                |> (\( model, cmd ) ->
+                        ( { session | pageModel = LoginModel model }
+                        , Cmd.map LoginMsg cmd
+                        , []
+                        )
+                   )
+
+        ( MobModel mobModel, MobMsg mobMsg ) ->
+            let
+                mobResult =
+                    Mob.Main.update mobMsg mobModel
+            in
+            ( { session | pageModel = MobModel mobResult.model }
+            , Cmd.map MobMsg mobResult.command
+            , mobResult.toast
+            )
+
+        _ ->
+            ( session, Cmd.none, [] )
+
+
+subscriptions : Sub Msg
+subscriptions =
+    Mob.Main.subscriptions |> Sub.map MobMsg
+
+
+eventsMapping : EventsMapping Msg
+eventsMapping =
+    EventsMapping.map MobMsg Mob.Main.eventsMapping
+
+
+pageTitle : Session -> String
+pageTitle model =
+    case model.pageModel of
+        LoginModel _ ->
+            Login.title
+
+        MobModel mobModel ->
+            Mob.Main.pageTitle mobModel
+
+
+pageBody : Session -> Html Msg
+pageBody model =
+    case model.pageModel of
+        LoginModel loginModel ->
+            Login.view loginModel |> Html.map LoginMsg
+
+        MobModel mobModel ->
+            Mob.Main.view mobModel model.url |> Html.map MobMsg
