2fdf25a21db04f78220d811e22302edc4c91bceb
hadrienmp <public@hadrienmp.fr>
2021-03-31 13:54:29 +0200

    Attempt to deploy on heroku

--- server/app.js
+++ server/app.js
@@ -28,9 +28,6 @@ function historize(room, message) {
 const port = process.env.PORT || 3000
 
 let publicDirPath = path.join(__dirname + "/../", 'public');
-console.log(publicDirPath);
-console.log(path.join(path.dirname(__dirname), 'public'));
-ls(publicDirPath);
 app.use(express.static(publicDirPath))
     .get('/', (req, res) => {
         res.sendFile(path.join(publicDirPath, "index.html"))
@@ -41,20 +38,3 @@ app.use(express.static(publicDirPath))
     .listen(port, () => {
         console.log(`Live at http://0.0.0.0:${port}`)
     });
-
-function ls(dir) {
-    // list all files in the directory
-    try {
-        const files = fs.readdirSync(dir);
-
-        // files object contains all files names
-        // log them on console
-        files.forEach(file => {
-            console.log(file);
-        });
-
-    } catch (err) {
-        console.log(err);
-    }
-}
-
