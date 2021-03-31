ca35a6d772fe13096078ecc5f1745a40db849364
hadrienmp <public@hadrienmp.fr>
2021-03-31 16:05:12 +0200

    Attempt to make socketio work on heroki

--- server/app.js
+++ server/app.js
@@ -1,31 +1,28 @@
 const express = require('express')
 const app = require('express')();
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const path = require('path');
-const fs = require('fs');
 
 const history = {}
 
-// io.on('connection', (socket) => {
-//     socket.on('join', room => {
-//         socket.join(room);
-//         socket.emit('history', history[room] ? history[room] : []);
-//     });
-//     socket.on('message', (room, message) => {
-//         historize(room, message);
-//         io.in(room).emit('message', message);
-//     });
-//     socket.on('disconnect', () => console.log('user disconnected'));
-// });
+io.on('connection', (socket) => {
+    socket.on('join', room => {
+        socket.join(room);
+        socket.emit('history', history[room] ? history[room] : []);
+    });
+    socket.on('message', (room, message) => {
+        historize(room, message);
+        io.in(room).emit('message', message);
+    });
+    socket.on('disconnect', () => console.log('user disconnected'));
+});
 
 function historize(room, message) {
     let roomHistory = history[room] || [];
     roomHistory.push(message);
     history[room] = roomHistory;
 }
-
-const port = process.env.PORT || 3000
 
 let publicDirPath = path.join(__dirname + "/../", 'public');
 app.use(express.static(publicDirPath))
@@ -35,6 +32,8 @@ app.use(express.static(publicDirPath))
     .get('/mob/*', (req, res) => {
         res.sendFile(path.join(publicDirPath, "index.html"))
     })
-    .listen(port, () => {
+
+const port = process.env.PORT || "3000"
+server.listen(port, () => {
         console.log(`Live at http://0.0.0.0:${port}`)
     });
