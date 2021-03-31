bc4d5cf2d462c4f275fff87a905abb13537888a4
hadrienmp <public@hadrienmp.fr>
2021-03-31 08:47:41 +0200

    Attempt to deploy on heroku

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 const path = require('path');
 
 const history = {}
 
-io.on('connection', (socket) => {
-    socket.on('join', room => {
-        socket.join(room);
-        socket.emit('history', history[room] ? history[room] : []);
-    });
-    socket.on('message', (room, message) => {
-        historize(room, message);
-        io.in(room).emit('message', message);
-    });
-    socket.on('disconnect', () => console.log('user disconnected'));
-});
+// io.on('connection', (socket) => {
+//     socket.on('join', room => {
+//         socket.join(room);
+//         socket.emit('history', history[room] ? history[room] : []);
+//     });
+//     socket.on('message', (room, message) => {
+//         historize(room, message);
+//         io.in(room).emit('message', message);
+//     });
+//     socket.on('disconnect', () => console.log('user disconnected'));
+// });
 
 function historize(room, message) {
     let roomHistory = history[room] || [];
@@ -30,7 +30,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(path.dirname(__dirname), 'public', "index.html"))
 })
 
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 
 http.listen(port, () => {
     console.log(`Live at http://0.0.0.0:${port}`)
