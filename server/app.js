cfcb170988de0a8744c677481283dec755e3fa48
hadrienmp <github@hadrienmp.fr>
2023-03-02 19:05:28 +0100

    🚨 remove ignored parameters

--- server/app.js
+++ server/app.js
@@ -7,41 +7,41 @@ const path = require('path');
 const history = {}
 
 io.on('connection', (socket) => {
-    socket.on('join', room => {
-        socket.join(room);
-        socket.emit('history', history[room] ? history[room] : []);
-    });
-    socket.on('message', (room, message) => {
-        historize(room, message);
-        io.in(room).emit('message', message);
-    });
-    socket.on('sync', (room, message) => {
-        let channel = socket.to(room);
-        if (message.recipient)
-            channel = io.to(message.recipient);
-        channel.emit('sync', message);
-    });
+  socket.on('join', room => {
+    socket.join(room);
+    socket.emit('history', history[room] ? history[room] : []);
+  });
+  socket.on('message', (room, message) => {
+    historize(room, message);
+    io.in(room).emit('message', message);
+  });
+  socket.on('sync', (room, message) => {
+    let channel = socket.to(room);
+    if (message.recipient)
+      channel = io.to(message.recipient);
+    channel.emit('sync', message);
+  });
 });
 
 function historize(room, message) {
-    let roomHistory = history[room] || [];
-    roomHistory.push(message);
-    history[room] = roomHistory;
+  let roomHistory = history[room] || [];
+  roomHistory.push(message);
+  history[room] = roomHistory;
 }
 
 let publicDirPath = path.join(__dirname + "/../", 'public');
 app.use(express.static(publicDirPath));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(publicDirPath, "index.html"))
-}).get('/mob/:mob', (req, res) => {
-    res.sendFile(path.join(publicDirPath, "index.html"))
-}).get('/mob/:mob/*', (req, res) => {
-    res.sendFile(path.join(publicDirPath, "index.html"))
-}).get('/me', (req, res) => {
-    res.sendFile(path.join(publicDirPath, "index.html"))
+app.get('/', (_, res) => {
+  res.sendFile(path.join(publicDirPath, "index.html"))
+}).get('/mob/:mob', (_, res) => {
+  res.sendFile(path.join(publicDirPath, "index.html"))
+}).get('/mob/:mob/*', (_, res) => {
+  res.sendFile(path.join(publicDirPath, "index.html"))
+}).get('/me', (_, res) => {
+  res.sendFile(path.join(publicDirPath, "index.html"))
 })
 
 const port = process.env.PORT || 3000
 server.listen(port, () => {
-    console.log(`Live at http://0.0.0.0:${port}`)
+  console.log(`Live at http://0.0.0.0:${port}`)
 });
