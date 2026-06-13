5b15fed1120990bd9d2ab24a75dcdb01ee506603
hadrienmp <github@hadrienmp.fr>
2026-06-13 18:59:45 +0200

    test(e2e): use page object models

---
+++ e2e/POMs.ts
@@ -0,0 +1,148 @@
+import { expect, type Page } from '@playwright/test';
+import { v4 as uuid } from 'uuid';
+
+const roles = ['Driver', 'Navigator', 'Next Navigator'];
+
+export class MobHome {
+    readonly page: Page;
+    readonly mobId: string;
+
+    constructor(page: Page, mobId: string) {
+        this.page = page;
+        this.mobId = mobId;
+    }
+
+    async goto() {
+        await this.page.goto(`http://localhost:3000/mob/${this.mobId}`);
+        return new Profile(this.page, this.mobId);
+    }
+
+    async expectTitlePage() {
+        await expect(this.page).toHaveTitle(`${this.mobId} | Mob Time`);
+    }
+
+    async gotoMobberSettings() {
+        await this.page.getByRole('button', { name: 'Add' }).click();
+        return new MobberSettings(this.page, this.mobId);
+    }
+    async gotoSettings() {
+        await this.page.getByRole('link', { name: 'Settings' }).click();
+        return new Settings(this.page, this.mobId);
+    }
+
+    async expectPlayersInOrder(playersInOrder: string[]) {
+        for (let i in playersInOrder) {
+            const player = playersInOrder[i];
+            const role = roles[i] ?? '';
+            await expect(this.page.getByText(`${role}${player}`)).toBeVisible();
+        }
+    }
+    getStartTurnButton() {
+        return this.page.getByRole('button', { name: 'Start a turn' });
+    }
+
+    async stopMusic() {
+        await this.page.getByRole('button', { name: 'Stop music' }).click();
+    }
+
+    async expectOvertime() {
+        await expect(this.page.getByText('Overtime')).toBeVisible();
+    }
+
+    async stopTurn() {
+        await this.page.getByRole('button', { name: 'Stop' }).first().click();
+    }
+}
+
+export class Profile {
+    readonly page: Page;
+    readonly mobId: string;
+
+    constructor(page: Page, mobId: string) {
+        this.page = page;
+        this.mobId = mobId;
+    }
+
+    async expectPageTitle() {
+        await expect(this.page).toHaveTitle(
+            `Profile | ${this.mobId} | Mob Time`,
+        );
+    }
+
+    async gotoMobHome() {
+        await this.page.getByRole('button', { name: 'Join' }).click();
+        return new MobHome(this.page, this.mobId);
+    }
+}
+
+export class Settings {
+    readonly page: Page;
+    readonly mobId: string;
+
+    constructor(page: Page, mobId: string) {
+        this.page = page;
+        this.mobId = mobId;
+    }
+
+    async toggleExtremeMode() {
+        await this.page.getByRole('button', { name: 'Extreme Mode' }).click();
+    }
+    async setTurnLength(value: number) {
+        await this.page
+            .getByRole('slider', { name: 'Turn' })
+            .fill(value.toString());
+    }
+    async goBackHome() {
+        await this.page.getByRole('button', { name: 'Back' }).click();
+        return new MobHome(this.page, this.mobId);
+    }
+}
+
+export class MobberSettings {
+    readonly page: Page;
+    readonly mobId: string;
+
+    constructor(page: Page, mobId: string) {
+        this.page = page;
+        this.mobId = mobId;
+    }
+
+    async expectTitlePage() {
+        await expect(this.page).toHaveTitle(
+            `Mobbers | ${this.mobId} | Mob Time`,
+        );
+    }
+
+    async goBackHome() {
+        await this.page.getByRole('button', { name: 'Back' }).click();
+        return new MobHome(this.page, this.mobId);
+    }
+
+    async addMobber(name: string) {
+        await this.page.getByRole('textbox', { name: 'Name' }).fill(name);
+        await this.page.getByRole('button', { name: 'Add' }).click();
+    }
+}
+
+export class AppHome {
+    readonly page: Page;
+    readonly testMobId = `test-${uuid()}`;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async goto() {
+        await this.page.goto('http://localhost:3000/');
+    }
+
+    async fillMobName() {
+        await this.page
+            .getByRole('textbox', { name: 'Name' })
+            .fill(this.testMobId);
+    }
+    async createMob() {
+        await this.page.getByRole('button', { name: 'Create' }).click();
+        return new MobHome(this.page, this.testMobId);
+    }
+}
--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,9 +1,8 @@
-import { Page } from '@playwright/test';
-import { test, expect } from './fixtures/audio';
 import { v4 as uuid } from 'uuid';
+import { AppHome, MobHome } from './POMs.ts';
+import { test, expect } from '@playwright/test';
 
 const testMobId = `test-${uuid()}`;
-const roles = ['Driver', 'Navigator', 'Next Navigator'];
 
 // TODO: Quand on lance un tour ca le lance chez tout le monde
 // TODO: Quand le tour se termine ca joue un son de la playlist selectionnee, le meme chez tout le monde
@@ -14,56 +13,47 @@ const roles = ['Driver', 'Navigator', 'Next Navigator'];
 // TODO: Quand on change les roles dans les settings ca change chez tout le monde
 // TODO: Quand on change la duree d'un tour ca change chez tout le monde
 test('has title', async ({ page }) => {
-    await page.goto(`http://localhost:3000/mob/${testMobId}`);
-    await expect(page).toHaveTitle(`Profile | ${testMobId} | Mob Time`);
-    await page.getByRole('button', { name: 'Test the audio' }).click();
-    await page.getByRole('button', { name: 'Join' }).click();
-    await expect(page).toHaveTitle(`${testMobId} | Mob Time`);
+    const mobHome = new MobHome(page, testMobId);
+    const profile = await mobHome.goto();
+    await profile.expectPageTitle();
+    await profile.gotoMobHome();
+    await mobHome.expectTitlePage();
 });
 
 test('test', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
-    await page.getByRole('textbox', { name: 'Name' }).fill(testMobId);
-    await page.getByRole('button', { name: 'Test the audio' }).click();
-    await page.getByRole('button', { name: 'Create' }).click();
-    await expect(page).toHaveTitle(`${testMobId} | Mob Time`);
+    const appHome = new AppHome(page);
+    await appHome.goto();
+    await appHome.fillMobName();
+    const mobHome = await appHome.createMob();
 
-    await page.getByRole('button', { name: 'Add' }).click();
-    await page.getByRole('textbox', { name: 'Name' }).fill('First P');
-    await page.getByRole('button', { name: 'Add' }).click();
-    await page.getByRole('textbox', { name: 'Name' }).fill('Second P');
-    await page.getByRole('button', { name: 'Add' }).click();
-    await page.getByRole('textbox', { name: 'Name' }).fill('Third P');
-    await page.getByRole('button', { name: 'Add' }).click();
-    await page.getByRole('textbox', { name: 'Name' }).fill('Fourth P');
-    await page.getByRole('button', { name: 'Add' }).click();
-    await page.getByRole('button', { name: 'Back' }).click();
+    await mobHome.expectTitlePage();
 
-    await expectPlayersInOrder(page, ['First', 'Second', 'Third', 'Fourth']);
-
-    await page.getByRole('link', { name: 'Settings' }).click();
-    await page.getByRole('button', { name: 'Extreme Mode' }).click();
-    await page.getByRole('slider', { name: 'Turn' }).fill('1');
-    await page.getByRole('button', { name: 'Back' }).click();
-    await page.getByRole('button', { name: 'Start a turn 00:01' }).click();
-    await page.getByRole('button', { name: 'Stop music' }).click();
-
-    await expect(page.getByText('Overtime')).toBeVisible();
-    await expect(
-        page.getByRole('button', { name: 'Start a turn' }),
-    ).not.toBeVisible();
-
-    await page.getByRole('button', { name: 'Stop' }).first().click();
-    await expect(
-        page.getByRole('button', { name: 'Start a turn' }),
-    ).toBeVisible();
-
-    await expectPlayersInOrder(page, ['Second', 'Third', 'Fourth', 'First']);
-});
-async function expectPlayersInOrder(page: Page, playersInOrder: string[]) {
-    for (let i in playersInOrder) {
-        const player = playersInOrder[i];
-        const role = roles[i] ?? '';
-        await expect(page.getByText(`${role}${player} P`)).toBeVisible();
+    const mobberSettings = await mobHome.gotoMobberSettings();
+    await mobberSettings.expectTitlePage();
+    const players = ['First', 'Second', 'Third', 'Fourth'];
+    for (let player of players) {
+        await mobberSettings.addMobber(player);
     }
-}
+    await mobberSettings.goBackHome();
+
+    await mobHome.expectPlayersInOrder(players);
+
+    const settings = await mobHome.gotoSettings();
+    await settings.toggleExtremeMode();
+    await settings.setTurnLength(1);
+    await settings.goBackHome();
+
+    const startTurnButton = mobHome.getStartTurnButton();
+    await expect(startTurnButton).toHaveText(/00:01/);
+    await startTurnButton.click();
+
+    await mobHome.stopMusic();
+    await mobHome.expectOvertime();
+
+    await expect(startTurnButton).not.toBeVisible();
+
+    mobHome.stopTurn();
+    await expect(startTurnButton).toBeVisible();
+
+    await mobHome.expectPlayersInOrder(['Second', 'Third', 'Fourth', 'First']);
+});
