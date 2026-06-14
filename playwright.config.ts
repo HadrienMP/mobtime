a2d276f7cf424cc4f5ea197672254a654aad42f5
hadrienmp <github@hadrienmp.fr>
2026-06-14 08:47:06 +0200

    chore: delete server

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'pnpm run dev:front',
+        command: 'pnpm start',
         url: 'http://localhost:5173',
         reuseExistingServer: !process.env.CI,
     },
